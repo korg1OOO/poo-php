be957c256aa7fe085ea45fde02992749813f1f87
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2024-09-13 15:47:02 -0300

    Update cartas.php

--- cartas.php
+++ cartas.php
@@ -10,12 +10,14 @@
 
         public function __toString()
         {
-            $this->numero . " de " . $this->nome;
+            return $this->numero . " de " . $this->nome;
         }
 
     }
 
     $cartas = [];
+    $palpite = "";
+    $pontuacao = 0;
 
     $naipes = ["Copas", "Espadas", "Ouros", "Paus"];
     $numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -27,8 +29,57 @@
         }
     }
 
-    $carta_escolhida = array_rand($cartas, 1);
+    $carta_escolhida = array_rand($cartas);
+    print($carta_escolhida);
+    foreach($cartas as $carta){
+        echo $carta . " - ";
+    }
 
-    foreach($cartas as $i =>$c){
-        
+    do {
+        echo "-----------JOGO-----------\n";
+        echo "1- Dar palpite\n";
+        echo "2- Ver chutes restantes\n";
+        echo "3- Pedir dica\n";
+        echo "4- Ver pontos\n";
+        echo "0- DESISTIR\n";
+        $opcao = readline("Escolha a opção: \n");
+
+    switch($opcao) {
+        case 0:
+            echo "Programa encerrado!\n";
+        break;
+
+        case 1:
+            $palpite = readline("Dê um palpite: \n");
+
+            if($palpite == $carta_escolhida){
+                echo "Parabéns você acertou!\n";
+                $pontuacao += 10;
+            } else {
+                echo "Que pena você errou!\n";
+            }
+        break;
+
+        case 2:
+            echo "Dica: \n";
+            //FALTA FAZER
+        break;
+
+        case 3:
+            echo "LISTANDO CARTAS RESTANTES...\n";
+            //FALTA FAZER
+        break;
+
+        case 4:
+            echo "CALCULANDO PONTOS...\n";
+            echo "Pontuação: $pontuacao\n";
+        break;
+
+        default:
+        echo "Opção INVÁLIDA!\n";
+        }
+
+    }
+
+    while($opcao != 0);){
     }
